4167dc04fe80efa368fbd5277c09077058cec701
Thibaud <thibaud29@protonmail.com>
2018-10-13 17:50:59 +0200

    sidebar

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,15 @@
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">Navigation</li>
+
+            <li>
+                <a href=" {{ route('home') }} ">
+                    <i class="fa fa-home"></i>
+                    <span> Home </span>
+                </a>
+            </li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
