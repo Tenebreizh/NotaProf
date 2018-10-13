014b89b3e05c54c8503f7b04e540d0944e3a8c13
Thibaud <thibaud29@protonmail.com>
2018-10-14 01:33:14 +0200

    hotfix home route link

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,13 +1,9 @@
 <header class="main-header">
-    <!-- Logo -->
-    <a href="index2.html" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
+    <a href=" {{ route('home') }} " class="logo">
         <span class="logo-mini"><b>N</b>tp</span>
         <span class="logo-lg"><b>Nota</b>Prof</span>
     </a>
-    <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-        <!-- Sidebar toggle button-->
         <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
       </a>
