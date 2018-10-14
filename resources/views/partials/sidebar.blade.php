e3eedfebecf17d4bbe19678554594055c6bac715
Thibaud <thibaud29@protonmail.com>
2018-10-14 23:34:57 +0200

    update sidebar icon

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,18 +5,17 @@
 
             <li>
                 <a href=" {{ route('home') }} ">
-                    <i class="fa fa-home"></i>
+                    <i class="fas fa-home"></i>
                     <span> Home </span>
                 </a>
             </li>
 
             <li>
                 <a href=" {{ route('sentences.index') }} ">
-                    <i class="fa fa-pen-nib"></i>
+                    <i class="fas fa-pen-nib"></i>
                     <span> Vos phrases </span>
                 </a>
             </li>
         </ul>
     </section>
-    <!-- /.sidebar -->
 </aside>
