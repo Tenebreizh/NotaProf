919465b9e97fc818f304f09ca08f0fef8531dbf7
Thibaud <thibaud29@protonmail.com>
2018-10-14 16:56:05 +0200

    create sentence controller, migration, model, factory & seed

---
+++ database/factories/SentenceFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Sentence::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word(),
+        'content' => $faker->sentence(rand(3, 12)),
+    ];
+});
---
+++ database/migrations/2018_10_14_144622_create_sentences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSentencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sentences', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('name');
+            $table->text('content');
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sentences');
+    }
+}
