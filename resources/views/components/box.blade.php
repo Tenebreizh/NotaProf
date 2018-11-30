3c970108e14b4ce1dd4768eee1eaf7777c7c5dd5
Thibaud <thibaud29@protonmail.com>
2018-11-30 22:32:15 +0100

    create box components
    To ease the creation of new boxes

---
+++ resources/views/components/box.blade.php
@@ -0,0 +1,9 @@
+<div class="box box-{{ $color }} ">
+    <div class="box-header with-border">
+        <h3 class="box-title"> {{ $title }} </h3>
+    </div>
+
+    <div class="box-body">
+        {{ $content }}
+    </div>
+</div>
