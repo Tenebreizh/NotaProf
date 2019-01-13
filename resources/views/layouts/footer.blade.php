f67bde4ef02ce6486bda5bc582ca42da436d2113
Thibaud <thibaud29@protonmail.com>
2019-01-13 12:23:09 +0100

    Update Year in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> {{ config('app.version') }}
     </div>
-    <strong>Copyright &copy; 2018 <a href="https://github.com/Tenebreizh">Thibaud Philippi</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; {{ \Carbon\Carbon::now()->format('Y') }} <a href="https://github.com/Tenebreizh">Thibaud Philippi</a>.</strong> All rights reserved.
 </footer>
